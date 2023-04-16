aa24f9a8f887a38f6f1e6cb943a57b0d965f2e45
mwalawski <michal.walawski@awesome-ind.com>
2023-04-16 15:58:19 +0200

    todo userpage

---
+++ src/UserPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h2>Witaj {{ authenticatedUsername }}</h2>
+      <a @click="logMeOut()">Wyloguj</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UserPage",
+    methods: {
+        enter() {
+            this.$emit('logout');
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
